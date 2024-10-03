f37a339c8f7b5f6619f52fef217afa9347e59db4
Edwinamos <aedwin0706@gmail.com>
2024-10-03 19:24:15 +0530

    Add files via upload

---
+++ day 12 Exercie.py
@@ -0,0 +1,10 @@
+'''
+tempVar="Hello World"
+print(tempVar.lower())
+print(tempVar.upper())
+print(tempVar.count('o'))
+print(tempVar.capitalize())
+'''
+tempVar="Hello World"
+print(tempVar.split()[0])
+
